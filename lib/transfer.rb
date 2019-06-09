fc5c5ebe8197d027088fe8ac8c63885ac1df778b
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 21:25:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,31 +14,19 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end
   
-  # def execute_transaction
-  #   if @status != "complete"
-  #     if valid? 
-  #       @sender.balance -= @amount 
-  #       @receiver.balance += @amount 
-  #       @status = "complete"
-  #       return nil
-  #     end
-  #   end
-  #   @status = "rejected"
-  #   "Transaction rejected. Please check your account balance."
-  # end
-  
   def execute_transaction
-    if self.status !="complete"
-        if valid?
-            @sender.balance-=@amount
-            @receiver.balance+=@amount
-              @status="complete"
-              return nil
-        end
+    if @status != "complete"
+      if valid? 
+        @sender.balance -= @amount 
+        @receiver.balance += @amount 
+        @status = "complete"
+        return nil
+      end
     end
-
-  @status="rejected"
-  return "Transaction rejected. Please check your account balance."
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
   end
+  
+  
   
 end
